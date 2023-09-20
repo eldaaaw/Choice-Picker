1856e2ca84e468ba480c2f457cdb30c1b7c592ce
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-20 09:44:04 +0300

    V.01

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Choice-Picker
-Randomly pick a choice 
+# Choice Picker
+
+Randomly pick a choice
+
+![](/Screenshot%202023-09-20%20at%209.30.08%20AM.png)
---
+++ script.js
@@ -0,0 +1,69 @@
+const textarea = document.getElementById("textarea");
+const tagsEl = document.getElementById("tags");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    //clear textarea when we hit Enter
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = " ";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerHTML = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  // how many times it will highligts before it stops
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlight(randomTag);
+
+    setTimeout(() => {
+      unHighlight(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlight(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlight(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlight(tag) {
+  tag.classList.remove("highlight");
+}
